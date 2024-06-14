c78dde11fc5b74ce823db5c5219b4b3dbdc3eadb
Murilo Damario <murilo.gcdamario@gmail.com>
2024-06-13 23:18:36 -0300

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
 require("express-async-errors")
-
+const cors = require('cors')
+const express = require("express")
 const migrationsRun = require("./database/sqlite/migrations")
 
-const AppError = require("./utils/AppError")
-
-const uploadConfig = require("./configs/upload")
-
-const express = require("express")
-
 const routes = require("./routes")
+const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
 migrationsRun()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
