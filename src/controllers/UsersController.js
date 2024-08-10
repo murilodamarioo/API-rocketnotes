87ce5543ff7f9dcaacb1c8139614013478aad9e2
Murilo Damario <murilo.gcdamario@gmail.com>
2024-08-10 00:03:03 -0300

    Implement User create test and refactor Controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,29 +1,17 @@
 const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
+const UserRepository = require("../repositories/UserRepository")
+const UserCreateService = require("../services/UserCreateService")
 
 class UsersController {
-    async index() {
-
-    }
-
-    async show() {
-
-    }
-
     async create(request, response) {
         const { name, email, password } = request.body
 
-        const database = await sqliteConnection()
-        const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+        const userRepository = new UserRepository()
+        const userCreateService = new UserCreateService(userRepository)
 
-        if (checkUserExists) {
-            throw new AppError("Este e-mail já está cadastrado")
-        }
-
-        const hashPassword = await hash(password, 8);
-
-        await database.run("INSERT INTO users (name, email, password) VALUES ((?), (?), (?))", [name, email, hashPassword])
+        await userCreateService.execute({ name, email, password })
 
         return response.status(201).json()
     }
---
+++ src/services/userCreateService.spec.js
@@ -0,0 +1,46 @@
+const UserCreateService = require('./UserCreateService')
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require('../utils/AppError')
+
+describe('UserCreateService', () => {
+  let userRepositoryInMemory = null
+  let userCreateService = null
+
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory()
+    userCreateService = new UserCreateService(userRepositoryInMemory)
+  })
+
+
+  it('should create user', async () => {
+    const user = {
+      name: 'Michael',
+      email: 'michael@email.com',
+      password: '123'
+    }
+  
+    const userCreated = await userCreateService.execute(user)
+  
+    expect(userCreated).toHaveProperty('id')
+  })
+
+  it('should not create user with existing email', async () => {
+    const user1 = {
+      name: 'User One',
+      email: 'userone@email.com',
+      password: '123'
+    }
+
+    const user2 = {
+      name: 'User Two',
+      email: 'userone@email.com',
+      password: '123'
+    }
+
+    await userCreateService.execute(user1)
+    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError('Este e-mail já está cadastrado'))
+  })
+})
+
+
+
